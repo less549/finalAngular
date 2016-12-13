03f6c371976b4e61e9b5714ae6bfcff5b267771a
less549 <less549@gmail.com>
2016-12-13 20:23:34 -0300

    extras

--- js/json.js
+++ js/json.js
@@ -1,19 +1,21 @@
 {"Challenges":[
     {"Name":"Season Challenges", "":""},
+    {"Name":"Beginner Challenges", "":""},
+    {"Name":"All Challenges", "":""},
 ]}
 
 {"Challenges":[
-	{"Name":"Winter Challenge", "":""},
-    {"Name":"Summer Challenge", "":""},
-    {"Name":"Holidays Challenge", "":""}
-    {"Name":"Fall Challenge", "":""}
-    {"Name":"Spring Challenge", "":""}
-    {"Name":"San Valentin Challenge", "":""}
-    {"Name":"100 poses Challenge", "":""}
-    {"Name":"Colors Challenge", "":""}
-    {"Name":"Shapes Challenge", "":""}
-    {"Name":"Literature Challenge", "":""}
-    {"Name":"Movie Challenge", "":""}
-    {"Name":"Holidays Challenge", "":""}
-    {"Name":"Holidays Challenge", "":""}
+	{"Name":"Winter Challenge", "time":""},
+    {"Name":"Summer Challenge", "time":""},
+    {"Name":"Holidays Challenge", "time":""}
+    {"Name":"Fall Challenge", "time":""}
+    {"Name":"Spring Challenge", "time":""}
+    {"Name":"San Valentin Challenge", "time":""}
+    {"Name":"100 poses Challenge", "time":"false", "beginner":"true"}
+    {"Name":"Colors Challenge", "time":"false"}
+    {"Name":"Shapes Challenge", "time":"false", "beginner":"true"}
+    {"Name":"Literature Challenge", "time":"false"}
+    {"Name":"Movie Challenge", "time":"false"}
+    {"Name":"Flower Challenge", "time":"false"}
+    {"Name":"Sports Challenge", "time":"false"}
 ]}
